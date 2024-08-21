357e542d43f4ca59af81f3964a5db93e233fd5e6
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-08-21 09:51:13 +0900

    first commit

---
+++ src/main/java/com/example/shoptry/ShopTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoptry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopTryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoptry/ShopTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoptry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
