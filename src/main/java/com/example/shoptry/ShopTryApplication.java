04136497f2f8aa9c4781c1930adb2db9e3ade850
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-08-21 10:43:41 +0900

    Add Development env

--- src/main/java/com/example/shoptry/ShopTryApplication.java
+++ src/main/java/com/example/shoptry/ShopTryApplication.java
@@ -2,12 +2,19 @@ package com.example.shoptry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShopTryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopTryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTryApplication.class, args);
+    }
 
+    @GetMapping(value="/")
+    public String HelloWorld(){
+        return "Hello World";
+    }
 }
